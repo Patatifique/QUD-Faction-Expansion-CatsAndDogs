dd766237b4626d5bcb9feae696aa8ee3473c9444
Patatifique <Patatifique@icloud.com>
2026-01-23 20:01:32 +0100

    added shrines for the gods

---
+++ Parts/Secrets/Brothers_CatGodWorldBuilderExtension.cs
@@ -0,0 +1,43 @@
+using XRL;
+using XRL.World;
+using XRL.World.WorldBuilders;
+using XRL.World.ZoneBuilders;
+
+namespace Brothers.CatsAndDogs
+{
+    [JoppaWorldBuilderExtension]
+    public class Brothers_CatGodWorldBuilderExtension  : IJoppaWorldBuilderExtension
+    {
+        public override void OnAfterBuild(JoppaWorldBuilder builder)
+        {
+            var location = builder.popMutableLocationOfTerrain("DesertCanyon", centerOnly: false);
+            var zoneID = builder.ZoneIDFromXY("JoppaWorld", location.X, location.Y);
+
+            var secret = builder.AddSecret(
+                zoneID,
+                "Miramihamia, Feline God's Shrine",
+                new string[2] { "lair", "cats" },
+                "Lairs",
+                "$brothers_catgod_lair"
+            );
+
+            var zoneManager = The.ZoneManager;
+
+
+            zoneManager.SetZoneProperty(zoneID, "SkipTerrainBuilders", true);
+            zoneManager.AddZonePostBuilder(zoneID, "MapBuilder", "FileName", "Brothers_ShrineCat.rpm");
+
+
+            // I might add a music later
+            //zoneManager.AddZonePostBuilder(zoneID, "Music", "Track", "Music/Bey Lah Heritage II");
+
+
+
+
+            // You can also set various properties on the zone, if you wish.
+            zoneManager.SetZoneName(zoneID, "lair of Miramihamia, Feline God", Article: "the", Proper: true);
+            zoneManager.SetZoneIncludeStratumInZoneDisplay(zoneID, false);
+            zoneManager.SetZoneProperty(zoneID, "NoBiomes", "Yes");
+        }
+    }
+}
---
+++ Parts/Secrets/Brothers_DogGodWorldBuilderExtension.cs
@@ -0,0 +1,43 @@
+using XRL;
+using XRL.World;
+using XRL.World.WorldBuilders;
+using XRL.World.ZoneBuilders;
+
+namespace Brothers.CatsAndDogs
+{
+    [JoppaWorldBuilderExtension]
+    public class Brothers_DogGodWorldBuilderExtension  : IJoppaWorldBuilderExtension
+    {
+        public override void OnAfterBuild(JoppaWorldBuilder builder)
+        {
+            var location = builder.popMutableLocationOfTerrain("Hills", centerOnly: false);
+            var zoneID = builder.ZoneIDFromXY("JoppaWorld", location.X, location.Y);
+
+            var secret = builder.AddSecret(
+                zoneID,
+                "Boroworof, Canine God's Shrine",
+                new string[2] { "lair", "dogs" },
+                "Lairs",
+                "$brothers_doggod_lair"
+            );
+
+            var zoneManager = The.ZoneManager;
+
+
+            zoneManager.SetZoneProperty(zoneID, "SkipTerrainBuilders", true);
+            zoneManager.AddZonePostBuilder(zoneID, "MapBuilder", "FileName", "Brothers_ShrineDog.rpm");
+
+
+            // I might add a music later
+            //zoneManager.AddZonePostBuilder(zoneID, "Music", "Track", "Music/Bey Lah Heritage II");
+
+
+
+
+            // You can also set various properties on the zone, if you wish.
+            zoneManager.SetZoneName(zoneID, "lair of Boroworof, Canine God", Article: "the", Proper: true);
+            zoneManager.SetZoneIncludeStratumInZoneDisplay(zoneID, false);
+            zoneManager.SetZoneProperty(zoneID, "NoBiomes", "Yes");
+        }
+    }
+}
