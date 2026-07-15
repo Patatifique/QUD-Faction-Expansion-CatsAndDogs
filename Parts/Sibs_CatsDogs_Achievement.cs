ab30cc2ed91d9f52b429b1b6269714f829091fb3
Balthichou <153240971+Balthichou@users.noreply.github.com>
2026-07-15 11:19:28 +0200

    achievement names and desc

--- Parts/Sibs_CatsDogs_Achievement.cs
+++ Parts/Sibs_CatsDogs_Achievement.cs
@@ -6,40 +6,40 @@ namespace XRL
   {
     public static readonly AchievementInfo SIBS_CATSDOGS_NEUTRAL_ENDING = new AchievementInfo(
       "ACH_SIBS_CATSDOGS_NEUTRAL_ENDING", 
-      "FE - Cats and Dogs : Status Quo", 
+      "FE - Cats and Dogs: Status Quo", 
       "welcome.png", 
-      "Maintain the fragile peace in the villages of Shik and Spar.");
+      "Maintain the fragile peace between the villages of Shik and Spar.");
  
     public static readonly AchievementInfo SIBS_CATSDOGS_SHIK_ENDING = new AchievementInfo(
       "ACH_SIBS_CATSDOGS_SHIK_ENDING", 
-      "FE - Cats and Dogs : The Dog's Bite", 
+      "FE - Cats and Dogs: The Dog's Bite", 
       "welcome.png", 
       "Settle the dispute in Shik and Spar in Shik's favor.");
 
     public static readonly AchievementInfo SIBS_CATSDOGS_SPAR_ENDING = new AchievementInfo(
       "ACH_SIBS_CATSDOGS_SPAR_ENDING", 
-      "FE - Cats and Dogs : The Cat's Claw", 
+      "FE - Cats and Dogs: The Cat's Claw", 
       "welcome.png", 
       "Settle the dispute in Shik and Spar in Spar's favor.");
 
     public static readonly AchievementInfo SIBS_CATSDOGS_PERFECT_ENDING = new AchievementInfo(
       "ACH_SIBS_CATSDOGS_PERFECT_ENDING", 
-      "FE - Cats and Dogs : Family", 
+      "FE - Cats and Dogs: Family", 
       "welcome.png", 
       "End the feud and reunite Shikspar.");
 
 
     public static readonly AchievementInfo SIBS_CATSDOGS_FERAL_DINNER = new AchievementInfo(
       "ACH_SIBS_CATSDOGS_FERAL_DINNER", 
-      "FE - Cats and Dogs : Gone Feral", 
+      "FE - Cats and Dogs: Gone Feral", 
       "UI/Achievements/catsdogs/feraldinner.png", 
       "Eat the Feral Dinner.");
 
    public static readonly AchievementInfo SIBS_CATSDOGS_HYDRA_DOG = new AchievementInfo(
       "ACH_SIBS_CATSDOGS_HYDRA_DOG", 
-      "FE - Cats and Dogs : The Pack Within", 
+      "FE - Cats and Dogs: The Pack Within", 
       "UI/Achievements/catsdogs/hydradog.png", 
-      "Cause a Hydric Hound to reach 10 heads.");
+      "Witness a hydric hound reaching 10 heads.");
 
 
     [ModSensitiveCacheInit]
